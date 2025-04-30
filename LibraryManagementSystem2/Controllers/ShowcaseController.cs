9e09920c3e59567eab44b67d56e301daac16301c
salihakapo <salihakapo@icloud.com>
2025-04-30 21:25:22 +0300

    showcase template

---
+++ LibraryManagementSystem2/Controllers/ShowcaseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManagementSystem2.Models.Entity;
+namespace LibraryManagementSystem2.Controllers
+{
+    public class ShowcaseController : Controller
+    {
+        // GET: Showcase
+        DBLIBRARY2Entities db = new DBLIBRARY2Entities();
+        public ActionResult Index()
+        {
+            var value = db.TBLBOOK.ToList();
+            return View(value);
+        }
+    }
+}
